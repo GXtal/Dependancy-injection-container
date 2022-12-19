5cdc718e09a8d407c5a2bf7266330c7e790ab0b6
GXtal <zazaza00133@gmail.com>
2022-12-19 14:21:29 +0300

    Create DependencyImplementation.cs
    class contains correspondence of dependancy with implementation

---
+++ DependencyInjectionContainer/DependencyInjectionContainer/DependencyImplementation.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DependencyInjectionContainer
+{
+    public class DependencyImplementation
+    {
+        public object SingletonImplementation { get; set; }
+
+        public Type DependencyType { get; }
+
+        public Type ImplementationType { get; }
+
+        public bool IsSingleton { get; set; }
+
+        public DependencyImplementation(Type dependency, Type implementation, bool isSingleton)
+        {
+            DependencyType = dependency;
+            ImplementationType = implementation;
+            IsSingleton = isSingleton;
+        }
+    }
+}
